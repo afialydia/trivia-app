702e78a852d56d09310dc18e895561cd53220fff
afialydia <afia.caruso@gmail.com>
2021-10-04 13:22:34 -0400

    Added ChakraUI, FramerMotion, Emotion + new fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 //libraries
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 //styles
-import './App.css';
+import "./App.css";
+import { Container, Image, Center, Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
 //files
-import GAME_PLAY from './pages/01_game_play.page';
-import GAME_SET_UP from './pages/00_game_set_up.page';
+import GAME_PLAY from "./pages/01_game_play.page";
+import GAME_SET_UP from "./pages/00_game_set_up.page";
+import SET_UP2 from "./pages/game_settings";
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<Container
+			centerContent
+			className="App"
+			maxW="container.full"
+			maxH="container.full"
+			background="rgb(244, 246, 222)"
+			backgroundSize="cover"
+			// minH='100vh'
+			// border="solid rgb(129,25,40)"
+		>
+			{/* <Image
+				src="/cowboy.svg"
+				boxSize={{ base: "xs" }}
+				alt="BoyHowdyTrivia"
+				margin="auto 0"
+			/> */}
+			{/* <Spacer /> */}
+			<Center>
+				<Switch>
+					<Route exact path="/" component={SET_UP2} />
 
-				<GAME_SET_UP />
-				<GAME_PLAY/>
-			
-			
-    </div>
-  );
+					<Route exact path="/game" component={GAME_PLAY} />
+					{/*       
+			<GAME_PLAY />
+			<GAME_SET_UP /> */}
+				</Switch>
+			</Center>
+			{/* <Box
+				w="100vw"
+				background="rgb(216,218,197)"
+				height="4vh"
+				border="none"
+			></Box> */}
+		</Container>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 //styles
 import "bootstrap/dist/css/bootstrap.min.css";
+import theme from "./theme.js";
 import "./index.css";
 
 //files
@@ -14,10 +17,16 @@ import { store } from "./redux/store";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>,
+	<AnimateSharedLayout type="crossfade">
+		<AnimatePresence exitBeforeEnter initial>
+			<ChakraProvider theme={theme}>
+				<Provider store={store}>
+					<Router>
+						<App />
+					</Router>
+				</Provider>
+			</ChakraProvider>
+		</AnimatePresence>
+	</AnimateSharedLayout>,
 	rootElement
 );
