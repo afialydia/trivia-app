57fae029c6b53842c287741126b7d50f8ef39ff4
afialydia <afia.caruso@gmail.com>
2021-10-04 18:58:20 -0400

    extracted game wall to its own component

--- src/pages/01_game_play.page.js
+++ src/pages/01_game_play.page.js
@@ -10,16 +10,10 @@ import Questions from "../components/questions";
 
 //styles
 import {
-	Link,
-	Text,
-	Heading,
-	chakra,
 	Center,
-	Image,
-	Flex,
-	Spacer,
-	Grid,
+	Flex
 } from "@chakra-ui/react";
+import GameWall from "../components/game_wall";
 
 const GAME_PLAY = ({ selectAllQuestions }) => {
 	return (
@@ -29,60 +23,7 @@ const GAME_PLAY = ({ selectAllQuestions }) => {
 					<Questions allQuestions={selectAllQuestions} />
 				</Flex>
 			) : (
-				<Grid
-					templateColumns="1fr"
-					h="100vh"
-					// border="solid red"
-					w="full"
-				>
-					<Spacer />
-					<Link
-						// border='solid green'
-						_hover={{
-							color: "rgb(129,25,40)",
-							textDecoration: "none",
-						}}
-						_active={{ color: "rgb(216,218,197)" }}
-						as={RouterLink}
-						to="/"
-					>
-						<Image
-							// border='solid blue'
-							src="/cowboy.svg"
-							boxSize={{ base: "auto" }}
-							objectFit="cover"
-							alt="BoyHowdyTrivia"
-							margin="0auto"
-							maxW="600px"
-							background="rgb(237,111,151)"
-							borderRadius="full"
-							shadow="xl"
-							display={{ md: "block" }}
-						/>
-
-						<Heading
-							// border="solid"
-							fontSize={{ base: "3xl", sm: "4xl" }}
-							fontFamily="Shrikhand"
-							letterSpacing="wider"
-							lineHeight="short"
-							fontWeight="hairline"
-							textAlign="center"
-							color="rgb(129,25,40)"
-						>
-							<chakra.span display={{ base: "block" }}>Boy Howdy </chakra.span>
-							<chakra.span display={{ base: "block" }}>
-								Trivia Bodanza{" "}
-							</chakra.span>
-						</Heading>
-						<Spacer />
-						<Text textAlign="center" fontWeight="bold" color="rgb(237,111,151)">
-							Looks like you haven't selected any questions? <br />
-							Click above to go to the home page.{" "}
-						</Text>
-					</Link>
-					<Spacer />
-				</Grid>
+				<GameWall />
 			)}
 		</Center>
 	);
